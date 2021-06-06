0cf0a63be6b8e6f5f53444fd68322b793aff1e45
yann <yann.furrer@epitech.digital>
2021-06-06 14:46:21 +0200

    all

---
+++ Python bot/bot.py
@@ -0,0 +1,68 @@
+from os import replace
+import tweepy
+from tweepy.models import SearchResults
+import request
+from function import api, search, tweet
+
+
+
+
+
+
+
+#while_count = 0
+#nb_tweet = 0
+search_result ="vide"
+
+
+# def tweet(name, url, in_reply_to_status_id):
+#     auto_populate_reply_matadata = True
+#     status = name+"    "+url
+#     api.update_status(status, in_reply_to_status_id, auto_populate_reply_matadata)
+#     return "message envoye"
+
+
+
+
+def execute(search, nb_tweet):
+    while_count = 0
+    
+    while while_count < nb_tweet:
+        
+        if "cocktail" in  search[while_count].text:
+            modify = search[while_count].text.replace("@ChefBot7 cocktail", "")
+            modify = modify[1:].replace(" ", "%")
+            name , url = request.api_cocktail(modify)
+            
+            tweet(search, name, url, while_count)
+            
+        if "soft" in  search:
+            modify = search.replace("@ChefBot7 soft", "")
+            modify = modify[1:].replace(" ", "%")
+            
+        if "food" in  search:
+            modify = search.replace("@ChefBot7 food", "")
+            modify = modify[1:].replace(" ", "%")
+            
+        if "vege" in  search:
+            modify = search.replace("@ChefBot7 vege", "")
+            modify = modify[1:].replace(" ", "%")
+            
+        else:
+            print("else",search[while_count].text)
+        print(while_count)
+        while_count += 1
+
+
+
+
+
+
+
+# def start():
+#     stop = False
+#     while(not stop):
+search_result, nb_tweet = search()
+execute(search_result, nb_tweet)
+
+# start()
